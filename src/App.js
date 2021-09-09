ddcdb0ae328aad3021457359a5635572efdb6ef9
javaIsland <javatechisland@gmail.com>
2021-09-08 22:33:04 -0700

    jumbtron updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
-import React from "react"
-import './App.css';
+import React from "react";
+import Jumbotron from "./Components/jumbotron";
 
 function App() {
   return (
     <div className="App">
-      Hello
+<Jumbotron/>
+     
     </div>
   );
 }
